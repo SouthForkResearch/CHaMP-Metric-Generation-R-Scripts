cab8678e681cb685d9fbed7698a93d2232dba76b
Matthew Nahorniak <matt@southforkresearch.org>
2017-01-23 14:09:56 -0800

    Adding large wood frequency scripts to the repository.  Two separate scripts are used - one for 2011-2013, another for 2014-present (2016) due to changes in the raw data tables.

---
+++ LWD/2011-2013/LWFreq.R
@@ -0,0 +1,69 @@
+# Script to Calculate Large Wood Frequencies (LWDFreq_Wet and LWDFreq_Bf)
+# Specific to 2011-2013 data tables
+# Matt Nahorniak
+# 1/23/2017
+
+
+# Read the raw data
+LWD = read.csv("LargeWoodyDebris.csv", header=T)
+
+# Split into wet and dry parts for the different metrics.
+LWD_Dry = LWD[LWD$LargeWoodType == "Dry",]
+LWD_Wet = LWD[LWD$LargeWoodType == "Wet",]
+
+# read this to get site length
+MVI = read.csv("MetricVisitInformation.csv", header=T)
+
+# Get VisitID's
+VisitIDs=levels(factor(LWD$VisitID))
+
+# Initialize results
+LWFreq_Wet = rep(NA, length(VisitIDs))
+LWFreq_Bf = rep(NA, length(VisitIDs))
+
+
+# Loop through all VisitIDs
+
+for (i in 1:length(VisitIDs)){
+print(paste(i, "of", length(VisitIDs)))
+
+# subset data to only visitID in question
+LWD_ID_Dry = LWD_Dry[LWD_Dry$VisitID == VisitIDs[i],]
+LWD_ID_Wet = LWD_Wet[LWD_Wet$VisitID == VisitIDs[i],]
+LWD_ID_Dry$VisitID
+
+# Pull total number of LW pieces from the total column.
+LWD_Dry_sum = sum(LWD_ID_Dry$SumLWDCount)
+LWD_Wet_sum = sum(LWD_ID_Wet$SumLWDCount)
+
+## This is what's in documentation.  It misses some columns.
+#LWD_Dry_sum = sum(c(LWD_ID_Dry$SmallSmall,LWD_ID_Dry$SmallMedium,
+#LWD_ID_Dry$SmallLarge,LWD_ID_Dry$MediumSmall,LWD_ID_Dry$MediumMedium,
+#LWD_ID_Dry$MediumLarge,LWD_ID_Dry$LargeSmall,LWD_ID_Dry$LargeMedium,
+#LWD_ID_Dry$LargeLarge))
+
+#LWD_Wet_sum = sum(c(LWD_ID_WEt$SmallSmall,LWD_ID_Wet$SmallMedium,
+#LWD_ID_Wet$SmallLarge,LWD_ID_Wet$MediumSmall,LWD_ID_Wet$MediumMedium,
+#LWD_ID_Wet$MediumLarge,LWD_ID_Wet$LargeSmall,LWD_ID_Wet$LargeMedium,
+#LWD_ID_Wet$LargeLarge))
+
+idx = match(VisitIDs[i], MVI$VisitID)
+# Here's what is says in the documentation
+SiteLengthBankfull = MVI$Lgth_Bf[idx]
+# Here's what works
+SiteLengthBankfull = MVI$Lgth_Wet[idx]
+
+
+# Documentation says to round off, actual results are not rounded off.
+#LWFreq_Wet[i]=round(100*LWD_Wet_sum/SiteLengthBankfull,3)
+LWFreq_Wet[i]=100*LWD_Wet_sum/SiteLengthBankfull
+#LWFreq_Bf[i]=round(100*((LWD_Dry_sum + LWD_Wet_sum)/SiteLengthBankfull),3)
+LWFreq_Bf[i]=100*((LWD_Dry_sum + LWD_Wet_sum)/SiteLengthBankfull)
+
+}
+
+LWFreq_Wet[is.na(LWFreq_Wet)] = 0
+LWFreq_Bf[is.na(LWFreq_Bf)] = 0
+
+results = data.frame("VisitID"=VisitIDs, LWFreq_Wet, LWFreq_Bf)
+write.csv(results, "LWFreq.csv", row.names=F)
---
+++ LWD/2014-2016/LWFreq.R
@@ -0,0 +1,62 @@
+# Script to Calculate Large Wood Frequencies (LWDFreq_Wet and LWDFreq_Bf)
+# Specific to 2014-2016 (and beyond) data tables
+# Matt Nahorniak
+# 1/23/2017
+
+# REad the data file
+LWD = read.csv("LargeWoodPiece.csv", header=T)
+
+# Split into wet and dry components
+LWD_Dry = LWD[LWD$LargeWoodType == "Dry",]
+LWD_Wet = LWD[LWD$LargeWoodType == "Wet",]
+
+# Read the metricVisitINformation file which contains the needed site length
+MVI = read.csv("MetricVisitInformation.csv", header=T)
+
+# Create list of all VisitIDs
+VisitIDs=levels(factor(LWD$VisitID))
+
+# Initialize results arrays.
+LWFreq_Wet = rep(NA, length(VisitIDs))
+LWFreq_Bf = rep(NA, length(VisitIDs))
+
+
+i=1
+# Loop through each VisitID
+for (i in 1:length(VisitIDs)){
+print(paste(i, "of", length(VisitIDs)))
+
+# Get results just for the VisitID in question
+LWD_ID_Dry = LWD_Dry[LWD_Dry$VisitID == VisitIDs[i],]
+LWD_ID_Wet = LWD_Wet[LWD_Wet$VisitID == VisitIDs[i],]
+
+# There is one row of data for each VisitID, so the number of rows equals
+# the number of pieces of large wood.
+LWD_Dry_sum = nrow(LWD_ID_Dry)
+LWD_Wet_sum = nrow(LWD_ID_Wet)
+
+# Find the corresponding row on metricvisitinformtion (MVI) to extract site length
+idx = match(VisitIDs[i], MVI$VisitID)
+
+# Here's what is says in the documentation - use bankfull site length!
+SiteLengthBankfull = MVI$Lgth_Bf[idx]
+# Here's what's needed to match results in cm.org - Wetted site length used
+SiteLengthBankfull = MVI$Lgth_Wet[idx]
+
+# Documentation says "round" but cm.org results aren't rounded
+#LWFreq_Wet[i]=round(100*LWD_Wet_sum/SiteLengthBankfull,3)
+#LWFreq_Bf[i]=round(100*((LWD_Dry_sum + LWD_Wet_sum)/SiteLengthBankfull),3)
+
+# Results aren't rounded
+LWFreq_Wet[i]=100*LWD_Wet_sum/SiteLengthBankfull
+LWFreq_Bf[i]=100*((LWD_Dry_sum + LWD_Wet_sum)/SiteLengthBankfull)
+
+}
+
+# NA values (no rows of data) should be zero LW
+LWFreq_Wet[is.na(LWFreq_Wet)] = 0
+LWFreq_Bf[is.na(LWFreq_Bf)] = 0
+
+# Write results file.
+results = data.frame("VisitID"=VisitIDs, LWFreq_Wet, LWFreq_Bf)
+write.csv(results, "LWFreq.csv", row.names=F)
